d9aa98a23f7df7a9eb187240c1ab8a8b811fa4fe
zrn-ns <5319256+zrn-ns@users.noreply.github.com>
2024-11-29 17:54:27 +0900

    Initial Commit

---
+++ PlaceholderImageExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceholderImageExampleApp
+//
+//  Created by zrn_ns on 2024/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
